6d0b4c0cdec7dd87c520bcda4bf623f50fdaf12b
Simplejash <Simplejash333@gmail.com>
2017-08-15 20:54:40 +0800

    Update app.py

--- app.py
+++ app.py
@@ -12,19 +12,19 @@ from linebot.models import (
 )
 
 # 引入 ChatBot
-from chatterbot import ChatBot
+#from chatterbot import ChatBot
 
 # 建立一個 ChatBot 物件
-chatbot = ChatBot(
-    'Ron Obvious',
-    trainer = 'chatterbot.trainers.ChatterBotCorpusTrainer'
-)
+# chatbot = ChatBot(
+#    'Ron Obvious',
+#    trainer = 'chatterbot.trainers.ChatterBotCorpusTrainer'
+#)
 
 # 基於英文的自動學習套件
-chatbot.train("chatterbot.corpus.english")
+# chatbot.train("chatterbot.corpus.english")
 
 # 與 ChatBot 對話，並且取得回應
-chatbot.get_response("Hello, how are you today?")
+# chatbot.get_response("Hello, how are you today?")
 
 app = Flask(__name__)
 
