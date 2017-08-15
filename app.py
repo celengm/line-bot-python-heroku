ae72a94b154d482a23a2dbaaa57a007171e567fc
Simplejash <Simplejash333@gmail.com>
2017-08-15 23:44:13 +0800

    Update app.py

--- app.py
+++ app.py
@@ -76,8 +76,8 @@ def callback():
 def handle_text_message(event):   
     text = event.message.text #message from user
     bot_response = chatbot.get_response(text)
-    if (bot_response == "1"):
-        text = "fffffff"    
+    if (bot_response == '1'):
+        text = 'fffffff'    
     line_bot_api.reply_message(
      event.reply_token,
      TextSendMessage(text= text)) #reply the same message from user
