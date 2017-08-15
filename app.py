7a435680dd2057ac323f6e185775589354b17ac6
Simplejash <Simplejash333@gmail.com>
2017-08-15 22:46:30 +0800

    Update app.py

--- app.py
+++ app.py
@@ -16,8 +16,8 @@ from chatterbot import ChatBot
 
 # 建立一個 ChatBot 物件
 chatbot = ChatBot(
-    'Johnson test',
-    trainer = 'chatterbot.trainers.ChatterBotCorpusTrainer'
+    'Johnson test' #,
+    #trainer = 'chatterbot.trainers.ChatterBotCorpusTrainer'
 )
 
 # 基於英文的自動學習套件
